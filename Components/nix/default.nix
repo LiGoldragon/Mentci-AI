a4e6bf6990b99cf2835fd380df05d08817f3ab2a
li <li@maisiliym.criome.net>
2026-02-26 22:14:29 +0100

    session: fix Gemini schema errors and optimize build pipeline

--- Components/nix/default.nix
+++ Components/nix/default.nix
@@ -73,9 +73,8 @@ let
   };
 
   vtcode = import ./vtcode.nix {
-    inherit pkgs;
+    inherit pkgs craneLib;
     src = vtcode_src;
-    rust_toolchain = rust_toolchain;
   };
 
   execute = pkgs.runCommand "mentci-execute" { } ''
